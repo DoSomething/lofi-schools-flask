ef82cd54f305f784f615cc2ddff3c024c64a375d
Matt Holford <mholford@dosomething.org>
2014-10-08 11:37:16 -0400

    Initial commit from current deployment

---
+++ lofi/config.py
@@ -0,0 +1,10 @@
+#LOFI_HOST = 'localhost'
+#LOFI_PORT = 5000
+# DEBUG = True
+# MONGODB_SETTINGS = {
+#   'DB': '',
+#   'USERNAME': '',
+#   'PASSWORD': '',
+#   'HOST': '',
+#   'PORT':
+# }
---
+++ lofi/main.py
@@ -0,0 +1,81 @@
+from flask import Flask, request
+import json, os
+
+app = Flask(__name__)
+
+# Load configuration file lofi/config.py
+app.config.from_object('config')
+#app.config['DEBUG'] = True
+
+# override variables with config file from local config file
+# if 'LOFI_CONFIG_FILE' in os.environ:
+#     app.config.from_envvar('LOFI_CONFIG_FILE')
+
+app.config['MONGODB_SETTINGS'] = {
+  'DB': os.environ['LOFI_DB_NAME'],
+  'USERNAME': os.environ['LOFI_DB_USERNAME'],
+  'PASSWORD': os.environ['LOFI_DB_PASSWORD'],
+  'HOST': os.environ['LOFI_DB_HOST'],
+  'PORT': int(os.environ['LOFI_DB_PORT'])
+}
+
+from models import db, Location
+db.init_app(app)
+
+@app.route('/search', methods=['GET'])
+def search():
+    if 'query' not in request.args or not request.args['query']:
+        return json.dumps({
+            'meta': {
+                'code': 400,
+                'errorType': 'param_error',
+                'errorDetail': 'Missing or empty query parameter'
+            },
+        }), 400
+
+    limit = 10
+    if 'limit' in request.args and request.args['limit'] and request.args['limit'].isdigit():
+        limit = int(request.args['limit'])
+
+    # Replacing whitespace with .* to offer more flexibility in what schools get
+    # returned in the search. ex: If the query is "Belleville High School", we'd
+    # still want it to return "Belleville Senior High School" as a result.
+    name_regex = request.args['query'];
+    name_regex = name_regex.replace(' ', '.*');
+
+    query = {'name':{'$regex': '%s' % name_regex, '$options': 'i'}};
+
+    if 'state' in request.args:
+        query['state'] = request.args['state'];
+
+    locations = Location.objects(__raw__=query).limit(limit)
+    results = []
+    for location in locations:
+        results.append(
+            {
+                'name': location.name,
+                'street': location.street,
+                'city': location.city,
+                'state': location.state,
+                'zip': location.zip,
+                'lat': location.lat,
+                'lon': location.lon,
+                'country': location.country,
+                'gsid': location.gsid
+            }
+        )
+    return json.dumps({
+        'meta': {
+            'code': 200
+        },
+        'results': results
+    }), 200
+
+port = os.environ.get('PORT', 5000)
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=int(port),
+        host='0.0.0.0'
+    )
